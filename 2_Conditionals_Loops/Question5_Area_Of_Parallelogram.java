29d0b2c47c263c2e0491d2a28d1c24c0e5c775a0
Aryan Agarwal <aryanagarwal607@gmail.com>
2022-12-21 17:48:26 +0530

    New solution added

---
+++ 2_Conditionals_Loops/Question5_Area_Of_Parallelogram.java
@@ -0,0 +1,15 @@
+package com.questions;
+
+import java.util.Scanner;
+
+public class Question5_Area_Of_Parallelogram {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the height of parallelogram: ");
+        int h = input.nextInt();
+        System.out.print("Enter the base length of parallelogram: ");
+        int b = input.nextInt();
+        double area = h * b;
+        System.out.println("Area of parallelogram is " + area);
+    }
+}
